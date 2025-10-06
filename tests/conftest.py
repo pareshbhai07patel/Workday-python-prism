ec30d1d616ff8759ff22a33e9cad478be69c98e1
pareshbhai07patel <pareshbhai07patel@gmail.com>
2025-10-06 17:50:12 -0400

    Update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,1 @@
-import os
-import pytest
 
-
-@pytest.fixture
-def rootdir():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-
-
-@pytest.fixture
-def schema_file(rootdir):
-    """Path to example JSON schema"""
-    return os.path.join(rootdir, "prism", "data", "schema.json")
